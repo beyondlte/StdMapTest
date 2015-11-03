bad4e3c3f697157a0fae264860e714512d3e22a3
JinghaoX <jinghao.xu@intusurg.com>
2015-11-03 14:59:44 -0800

    Map with comparison

--- MapTest/MapTest.cpp
+++ MapTest/MapTest.cpp
@@ -6,22 +6,53 @@
 #include <map>
 using namespace std;
 
+// define a compare function
+bool fncomp(char lhs, char rhs)
+{
+	return lhs < rhs;
+}
+
+// define a compare structure
+struct classcomp{
+	bool operator() (const char* lhs, const char* rhs) const
+	{
+		// lhs > rhs: 1
+		// lhs < rhs: -1
+		// lhs == rhs: 0
+		return strcmp(lhs, rhs) > 0;
+	}
+};
 int _tmain(int argc, _TCHAR* argv[])
 {
 	// declare a map
-	std::map<char, int> firstMap;
+	// by default, the map is not sorted
+	std::map<const char*, int> firstMap;
 	// initialize the map
-	firstMap['a'] = 10;
-	firstMap['b'] = 30;
-	firstMap['c'] = 50;
-	firstMap['d'] = 70;
+	firstMap["Z"] = 30;
+	firstMap["b"] = 10;
+	firstMap["y"] = 50;
+	firstMap["d"] = 70;
 
-	std::map<char, int> secondMap (firstMap.begin(), firstMap.end());
+	std::map<const char*, int> secondMap(firstMap.begin(), firstMap.end());
 	// loop through the map
-	std::map<char, int>::iterator it;
+	std::map<const char*, int>::iterator it;
 	for (it = secondMap.begin(); it != secondMap.end(); it++)
 	{
 		std::cout << it->first << ": " << it->second << "\n";
+	}
+
+	// define thirdMap with a sorted order using classcomp, here classcomp sorted the value in alphabet order
+	std::map<const char*, int, classcomp> thirdMap;
+	thirdMap["z"] = 10;
+	thirdMap["b"] = 20;
+	thirdMap["y"] = 50;
+	thirdMap["d"] = 40;
+
+	cout << "thirdMap in sorted:\n";
+	std::map<const char*, int, classcomp>::iterator cur;
+	for (cur = thirdMap.begin(); cur != thirdMap.end(); cur++)
+	{
+		std::cout << cur->first << ": " << cur->second << "\n";
 	}
 
 	return 0;
