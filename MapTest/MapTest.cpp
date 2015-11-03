557a660e5ba56e472d6136db7686729ddd8f5753
JinghaoX <jinghao.xu@intusurg.com>
2015-11-03 10:53:43 -0800

    init a map and loop through it

--- MapTest/MapTest.cpp
+++ MapTest/MapTest.cpp
@@ -2,10 +2,28 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <map>
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	// declare a map
+	std::map<char, int> firstMap;
+	// initialize the map
+	firstMap['a'] = 10;
+	firstMap['b'] = 30;
+	firstMap['c'] = 50;
+	firstMap['d'] = 70;
+
+	std::map<char, int> secondMap (firstMap.begin(), firstMap.end());
+	// loop through the map
+	std::map<char, int>::iterator it;
+	for (it = secondMap.begin(); it != secondMap.end(); it++)
+	{
+		std::cout << it->first << ": " << it->second << "\n";
+	}
+
 	return 0;
 }
 
